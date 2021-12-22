468e196ba279675933b21ee1ea73bb0ced674a01
Amir A. Shabani <amirashabani@zoho.com>
2021-12-22 13:24:24 +0330

    Add new page, statsfm

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
+def statsfm_view(request, *args, **kwargs):
+    return render(request, "statsfm.html", {})
+
--- src/shabani/urls.py
+++ src/shabani/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from pages.views import home_view
+from pages.views import home_view, statsfm_view
 
 urlpatterns = [
     path('', home_view),
+    path('statsfm', statsfm_view),
 ]
 
 if settings.ADMIN_ENABLED:
