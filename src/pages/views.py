6b51d407e35b264e85659d6a09f1189480119e54
Amir A. Shabani <amirashabani@zoho.com>
2022-01-17 23:40:58 +0330

    Two lines between functions and imports

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .forms import UsernameForm
 
+
 def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
+
 
 def statsfm_view(request, *args, **kwargs):
     if request.method == "GET":
